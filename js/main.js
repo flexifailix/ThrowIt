b4dd77f22a790e52e5dfe80425cb5878056d1b1c
flexifailix <felix.fritzsche.ext@W7-DEPDB9806.wincor-nixdorf.com>
2014-11-20 13:38:39 +0100

    first throws available

--- js/main.js
+++ js/main.js
@@ -3,18 +3,50 @@ var context;
 var canvasHeight;
 var canvasWidth;
 
+var frameRate = 60;
+
+var gravity = 10 / 1000 * 60;
+
 var isPlaying = false;
 
 var isDiscClicked = false;
 
-var posX = 50;
-var posY = 50;
+var playingDisc = {
+    x:20,
+    y:20,
+    accX:0,
+    accY:gravity,
+    radius:12,
+    
+    isCollision : function(posX, posY) {
+        if (posX === null)
+            posX = this.x;
+        
+        if (posY === null)
+            posY = this.y;
+        if (posX <= (this.x + this.radius) && posX >= this.x
+           && posY <= (this.y + this.radius) && posY >= this.y) {
+            return true;   
+        }
+        return false;
+    },
+    
+    resetAcc : function() {
+        this.accX = 0;
+        this.accY = gravity;
+    },
+    
+    resetPosition : function() {
+        this.x = 20;
+        this.y = 20;
+    }
+}
 
 var mouseOldPosX;
 var mouseOldPosY;
 
-var mouseAccelerationX;
-var mouseAccelerationY;
+var mouseAccelerationX = 0;
+var mouseAccelerationY = 0;
 
 init();
 function init()
@@ -39,33 +71,58 @@ function loop()
     }
     context.clearRect(0,0,canvasWidth,canvasHeight);
     
-    context.fillStyle = 'red';
-    context.fillRect(posX, posY, 50, 50);
+    if (!isDiscClicked) {
+        playingDisc.accY = playingDisc.accY + Math.round(gravity);
+                
+        playingDisc.x = playingDisc.x + playingDisc.accX;
+        playingDisc.y = playingDisc.y + playingDisc.accY;
+    }
     
-    window.setTimeout(loop, 1000/60);
+    if (playingDisc.y + playingDisc.radius > canvasHeight) {
+        playingDisc.y = canvasHeight - playingDisc.radius;
+        playingDisc.resetAcc();
+    }
+    
+    if (playingDisc.x - playingDisc.radius < 0) {
+        playingDisc.x = 0 + playingDisc.radius;
+        playingDisc.accX = 0;
+    }
+    
+    if (playingDisc.x + playingDisc.radius > canvasWidth) {
+        playingDisc.x = canvasWidth - playingDisc.radius;
+        playingDisc.accX = 0;
+    }
+    
+    
+    context.beginPath();
+    context.arc(playingDisc.x, playingDisc.y, playingDisc.radius, 0, 2 * Math.PI, false);
+    context.fillStyle = 'red';
+    context.fill();  
+        
+    window.setTimeout(loop, 1000 / frameRate);
 };
 
 function mouseMoveListener(e)
 {
     var mouseNewTimeStamp = new Date().getTime();
-    var x = e.pageX - document.getElementById('gameStage').offsetLeft;
-    var y = e.pageY - document.getElementById('gameStage').offsetTop;
-    document.getElementById('x').innerHTML = x;
-    document.getElementById('y').innerHTML = y;
+    var mouseNewPosX = e.pageX - document.getElementById('gameStage').offsetLeft;
+    var mouseNewPosY = e.pageY - document.getElementById('gameStage').offsetTop;
+    document.getElementById('x').innerHTML = mouseNewPosX;
+    document.getElementById('y').innerHTML = mouseNewPosY;
     
     if (isDiscClicked) {
         if (mouseOldPosX != null && mouseOldPosY != null) {
             var diffTimeStamp = mouseNewTimeStamp - mouseOldTimeStamp;
-            var diffPosX = mouseOldPosX - x;
-            var diffPosY = mouseOldPosY - y;
+            var diffPosX = mouseNewPosX - mouseOldPosX;
+            var diffPosY = mouseNewPosY - mouseOldPosY;
 
-            mouseAccelerationX = diffPosX / diffTimeStamp;
-            mouseAccelerationY = diffPosY / diffTimeStamp;
+            mouseAccelerationX = Math.round((diffPosX / diffTimeStamp) * 1000 / frameRate);
+            mouseAccelerationY = Math.round((diffPosY / diffTimeStamp) * 1000 / frameRate);
         }
         
         mouseOldTimeStamp = mouseNewTimeStamp;
-        mouseOldPosX = posX = x;
-        mouseOldPosY = posY = y;
+        mouseOldPosX = playingDisc.x = mouseNewPosX;
+        mouseOldPosY = playingDisc.y = mouseNewPosY;
     }
 };
 
@@ -76,6 +133,7 @@ function keyDownListener(event)
             isPlaying = false
         } else {
             isPlaying = true;
+            resetGame();
             loop();
         }
     }
@@ -83,22 +141,47 @@ function keyDownListener(event)
 
 function mouseDownListener(event)
 {
-    console.log("mouseDown");
+    resetAccData();
+    checkDiscClick(event);
+};
+
+function checkDiscClick(event)
+{
+    if (playingDisc.isCollision(event.pageX, event.pageY)) {
+        isDiscClicked = true;    
+    } else {
+        isDiscClicked = false;   
+    }
+}
+
+function resetGame()
+{
+    resetAccData();   
+    playingDisc.resetPosition();
+}
+
+function resetAccData()
+{
     mouseOldPosX = null;
     mouseOldPosY = null;
     mouseOldTimeStamp = null;
-    isDiscClicked = true; 
-};
+    mouseAccelerationX = 0;
+    mouseAccelerationY = 0;
+    
+    playingDisc.resetAcc();
+}
 
 function mouseUpListener(event)
 {
-    console.log("mouseUp");
-    console.log("mouseAcc x:" + mouseAccelerationX + " y: " + mouseAccelerationY);
-    isDiscClicked = false;
+    if (isDiscClicked) {
+        playingDisc.accX = playingDisc.accX + mouseAccelerationX;
+        playingDisc.accY = playingDisc.accY + mouseAccelerationY;
+        isDiscClicked = false;
+    }
+    
 };
 
 function mouseOutListener(event)
 {
-    console.log("mouseOut");
     isDiscClicked = false;
 };
